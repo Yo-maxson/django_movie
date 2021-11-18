d22238752e4551a7c332e9b39177a9fe88110711
Max Silchenko <maksim379@yandex.ru>
2021-11-18 23:10:06 +0300

    Добавление фильтра по году и жанру

--- movies/views.py
+++ movies/views.py
@@ -1,31 +1,31 @@
-from django.shortcuts import redirect, render
-from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.views.generic.base import View
 
-from .models import Movie, Category, Actor
+from .models import Movie, Category, Actor, Genre
 from .forms import ReviewForm
 
 
+class GenreYear:
+    """Жанры и года выхода фильмов"""
+    def get_genres(self):
+        return Genre.objects.all()
 
-# Create your views here.
-class MoviesView(ListView):
+    def get_years(self):
+        return Movie.objects.filter(draft=False).values("year")
+
+
+class MoviesView(GenreYear, ListView):
     """Список фильмов"""
     model = Movie
     queryset = Movie.objects.filter(draft=False)
 
 
-
-
-
-class MovieDetailView(DetailView):
+class MovieDetailView(GenreYear, DetailView):
     """Полное описание фильма"""
     model = Movie
     slug_field = "url"
-
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context["categories"] = Category.objects.all()
-        return context
 
 
 class AddReview(View):
@@ -42,9 +42,18 @@ class AddReview(View):
         return redirect(movie.get_absolute_url())
 
 
-
-class ActorView(DetailView):
+class ActorView(GenreYear, DetailView):
     """Вывод информации о актере"""
     model = Actor
-    template_name = "movies/actor.html"
+    template_name = 'movies/actor.html'
     slug_field = "name"
+
+
+class FilterMoviesView(GenreYear, ListView):
+    """Фильтр фильмов"""
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            Q(year__in=self.request.GET.getlist("year")) |
+            Q(genres__in=self.request.GET.getlist("genre"))
+        )
+        return queryset
